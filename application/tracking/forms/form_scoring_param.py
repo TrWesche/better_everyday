c12bc826d52d29efdfd8e5712e18edfc6fe9946a
Trenton Wesche <TrWesche@live.com>
2020-08-22 21:38:55 -0500

    Added tooltips for scoring system parameters form

--- application/tracking/forms/form_scoring_param.py
+++ application/tracking/forms/form_scoring_param.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, FloatField
-from wtforms.validators import DataRequired, Length, InputRequired
+from wtforms.validators import Length, InputRequired
 
 
 class ScoringSystemParamForm(FlaskForm):
     """Form for creating & updating user goals."""
 
-    score_input = FloatField('Score Input', validators=[InputRequired()])
-    score_output = FloatField('Score Output', validators=[InputRequired()])
-    name_en = StringField('Special Label', validators=[Length(max=50)])
+    score_input = FloatField('Time Spent (mins) *', validators=[InputRequired()], description='The time spent input which will result in the output Numerical Score (User input while tracking progress).')
+    score_output = FloatField('Calculated Score *', validators=[InputRequired()], description='The numerical score which the Time Spent input will result in (Calculated value).')
+    name_en = StringField('Text Label', validators=[Length(max=50)], description='An encouraging message or text label (ex. A,B,C,D,F) which will be displayed for this score.')
