883804e87d302c37bb6e2f251edc944fd1f4696d
Trenton Wesche <TrWesche@live.com>
2020-08-25 22:40:20 -0500

    trial db and demo acct management

--- clock.py
+++ clock.py
@@ -3,7 +3,7 @@ from seed_demo import seed_demo
 
 seedSched = BlockingScheduler()
 
-@seedSched.scheduled_job('interval', hour = 6)
+@seedSched.scheduled_job('interval', id='db_clean_demo_users', hours=6)
 def cleanupDatabase():
     print("Cleaning up database and reseeding with demo account data.")
     seed_demo()
